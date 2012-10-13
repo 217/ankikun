7b21a64a7308b2271714806d0d8e4e6aab29535f
Tetsunari Niina <konabanna@gmail.com>
2012-10-13 15:03:51 +0900

    Contorllerの作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ankikun::Application.config.secret_token = '70fd63ea5c918fb1dbd8ec884d497d02280f6dc35260a2de860326215252a92bf48cca71ecf8107364d2828a0868675c14faa3497ac528d6254a7e6071da8c48'
