f27d027eb4686cf9387a4c377966d794aed88fac
Tetsunari Niina <konabanna@gmail.com>
2012-10-14 09:30:12 +0900

    wiki/destroyの実装途中.

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/ruby
 # -*- coding:utf-8 -*-
 class WikiController < ApplicationController
   def index
@@ -28,7 +29,18 @@ class WikiController < ApplicationController
 	end
 
   def destroy
-		
+		@wiki = Wiki.find(:first,:conditions => {:wiki_id => params[:id],:story_id => params[:sub_id]}) 
+
+		if 'destroy_button' == params[:commit] then
+			@wiki.destroy
+			render :text => '<h1>削除しました。3秒後、移動します。</h1>'
+			sleep 3
+			redirect_to :wiki and return
+			return
+		else # if 'back_button' == params[:commit] then
+			render :text => '<h1>3秒後、トップページに移動します。</h1>'
+			sleep 3
+			redirect_to :wiki and return
+		end
   end
 end
-
