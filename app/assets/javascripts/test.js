adfb1cc3848a96bc580ccb5cee0a841336fef7e9
Tetsunari Niina <konabanna@gmail.com>
2012-10-18 01:13:46 +0900

    JavaScriptを使った文字列の表示に成功

---
+++ app/assets/javascripts/test.js
@@ -0,0 +1,7 @@
+function getSelect(){
+	var select = document.getElementById("test_questions_kind");
+	var options = document.getElementById("test_questions_kind").options;
+	var value = options.item(select.selectdindex).value;
+
+	document.write(value);
+}
