13f784f83c6af10cec04c7c4f767a59966a4b8e4
Tetsunari Niina <konabanna@gmail.com>
2012-10-19 19:02:22 +0900

    Deviseのエラーの解消

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,9 @@ ActiveRecord::Schema.define(:version => 20121019045150) do
     t.string   "last_sign_in_ip"
     t.integer  "omniuser_id"
     t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
   end
