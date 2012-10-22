15fe6cf3df956e96a012513152013b46040400a6
Tetsunari Niina <konabanna@gmail.com>
2012-10-23 02:07:10 +0900

    さっきの修正版

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -4,8 +4,9 @@ $(function(){
 	// セレクトを一括取得
 	$("select").each(function(){
 		// /^test_questions_kind[0-9]*$/ にヒットするものを実行
-		$(this).attr("id").match(/^test_questions_kind[0-9]*$/).live(function(){
-			
+		$("#" + $(this).attr("id").match(/^test_questions_kind[0-9]*$/)).live("change",function(){
+			alert($(this).val());
+			// alert(typeof($this));
 		});
 	});
 });
