dca9c464297ced10654e5658da69d4a1d0daf468
Tetsunari Niina <konabanna@gmail.com>
2012-10-24 12:16:44 +0900

    最新フォームでのN択問題の生成が完了

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -16,27 +16,23 @@ $(function(){
 	}
 
 	// N択問題の選択肢の生成
-	function createChoices(choiceNum,questionNum){
-		var choiceNum = new Number(choiceNum);
+	function createChoices(questionNum){
 		var questionNum = new Number(questionNum);
-		// 選択肢数
-		var choice_num = $("#test_questions_sub_kind").val();
+		var choiceNum = $("#test_questions_sub_kind" + questionNum).val();
 
-		/*
 		// 選択肢の数だけフォームを生成
-		for(var i = 1;i <= choice_num;i++){
+		for(var i = 1;i <= choiceNum;i++){
 			if(i === 1){
-				$("#test_body").after("<br><div id = \"choice\">1つ目の選択肢 : <input type = \"text\", id = \"test_questions_choices_choice1\", size = \"128\", maxlinght = \"256\", name = \"test[questions][choices][choice]\"></input> <input type = \"checkbox\" id = \"test_questions_choices_right1\", name=\"test[questions][choices][right]\"></input>");
+				$("#test_body" + questionNum).after("<br><div id = \"choice" + questionNum + "\">1つ目の選択肢 : <input type = \"text\", id = \"test_questions_choices_choice" + questionNum + "_1\", size = \"128\", maxlinght = \"256\", name = \"test[questions][choices][choice]\"></input> <input type = \"checkbox\" id = \"test_questions_choices_right" + questionNum +"_1\", name=\"test[questions][choices][right]\"></input>");
 			}else{
-				$("#test_questions_choices_right" + (i - 1)).after("<br>" + i + "つ目の選択肢 : <input type = \"text\", id = \"test_questions_choices_choice" + i + "\", size = \"128\", maxlinght = \"256\", name = \"test[questions][choices][choice]\" /> <input type = \"checkbox\" id = \"test_questions_choices_right" + i + "\", name=\"test[questions][choices][right]\" />");
+				$("#test_questions_choices_right" + questionNum + "_" + (i - 1)).after("<br>" + i + "つ目の選択肢 : <input type = \"text\", id = \"test_questions_choices_choice" + questionNum + "_" + i + "\", size = \"128\", maxlinght = \"256\", name = \"test[questions][choices][choice]\" /> <input type = \"checkbox\" id = \"test_questions_choices_right" + questionNum + "_" + i + "\", name=\"test[questions][choices][right]\" />");
 			}
 
 			// divの終了タグと送信ボタン
 			// ここも、===ではなく、==。(文字と数値の為)
-			if(i == choice_num){
-				$("#test_questions_choices_right" + choice_num).after("</div><br><input name=\"commit\" type=\"submit\" value=\"送信\" /><input type = \"button\" value = \"問題を追加\" id = \"add\" />");
+			if(i == choiceNum){
+				$("#test_questions_choices_right" + questionNum + "_" + choiceNum).after("</div><br><input name=\"commit\" type=\"submit\" value=\"送信\" /><input type = \"button\" value = \"問題を追加\" id = \"add\" />");
 			}
-			*/
 		}
 	}
 
@@ -48,6 +44,9 @@ $(function(){
 		if(selectText === "N択問題"){
 			$(this).after("<div id = \"sub_kind1\">何択か : <input type = \"text\" id = \"test_questions_sub_kind1\" size = \"2\", maxlinght = \"2\" name = \"test[questions][sub_kind]\" /></div>");	
 			createTextArea(selectText,1);
+			$("#test_questions_sub_kind1").bind("change",function(){
+				createChoices(1);
+			});
 		}else if(selectText === "○×問題"){
 
 		}else if(selectText === "一問一答"){
