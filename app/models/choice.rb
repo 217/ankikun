03a6e822dce556d0ad50d640ff4f6533f3032e69
Tetsunari Niina <konabanna@gmail.com>
2012-11-22 00:53:47 +0900

    複雑なvaildateを教えてもらいました。

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,5 +1,13 @@
 class Choice < ActiveRecord::Base
 	belongs_to :question
 
+  validate :validate_choice_text_presence
+
 	attr_accessible :choice_text, :right, :question_id, :choice_id, :exam_id
+private
+  def validate_choice_text_presence
+    if (question.kind == 1 || question.kind == 2) && choice_text.empty?
+      errors.add(:choice_text, "nothing")
+    end
+  end
 end
