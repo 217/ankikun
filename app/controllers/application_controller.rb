43f77f6a656951977c54426e343fb703a7b6ca2e
217 <konabanna@gmail.com>
2012-11-26 07:50:52 +0900

    Herokuなど用に設定をしたが破棄

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,8 @@
 class ApplicationController < ActionController::Base
-# Omniauthでのログイン状況確認
-private
-    def current_omniuser
-        @current_omniuser ||= Omniuser.find(session[:user_id]) if session[:user_id]
-    end
-
-  # http://d.hatena.ne.jp/Climber/20100527/1274942772
-  # 例外とステータスの関連付け
-  rescue_responses.update({
-    'Foo::BarError' => :not_found
-  })
-protected
-  # 確認をhttp://localhost 等で確認するときはコメントアウト
-  # が定石
-  #def local_request?
-  #  false
-  #end
-
-  def rescue_action_in_public(exception) #:doc:
-    status_message_sym = response_code_for_rescue(exception) || :internal_server_error
-    @http_status_code = SYMBOL_TO_STATUS_CODE[status_message_sym]
-    
-    respond_to do |format|
-      format.html { render :template => "commons/error.html", :layout => 'application', :status => @http_status_code }
-    end
+  # Omniauthでのログイン状況確認
+  def current_omniuser
+      @current_omniuser ||= Omniuser.find(session[:user_id]) if session[:user_id]
   end
-public
   protect_from_forgery
 
   helper_method :current_omniuser
